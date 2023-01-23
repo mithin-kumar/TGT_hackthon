adbfbf976a818d5147211a6d525ece2b704d7cc2
mithin-kumar <82752630+mithin-kumar@users.noreply.github.com>
2023-01-24 00:15:57 +0530

    final edit

--- model.py
+++ model.py
@@ -17,25 +17,33 @@ execution_path = os.getcwd()
 
 
 cap = cv2.VideoCapture(0)
+
+#creating the already trained model using imageai library
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(execution_path, "yolov3.pt"))
+
+#chooseing objects  to detect
 custom = detector.CustomObjects(
     person=True, dog=True,   bicycle=True,   car=True, bus=True,  truck=True)
 
-
+#mentioning our camera focal length
 focal_len = 3.1
-known_width = 10
-speed_us = 0.01
 
+#to calculate the distance of object from camera we need to know the actual width of image here we have taken an approximation
+known_width = 10 
+
+#to track the relative velocity initialise
 last_distance = 0
+safe_distance =0.06
+driver_speed =0.04
 
 
 while True:
-    print("capturing")
+    #capturing webcam 
     success, img = cap.read()
-#     print(success)
-#     cv2.imshow("image",img)
+    
+    #detecting objects in the image
     detections = detector.detectObjectsFromImage(input_image=img,
                                                  output_image_path=os.path.join(
                                                      execution_path, "imagenew.jpg"),
@@ -45,23 +53,21 @@ while True:
         width = box_pints[2]-box_pints[0]
 
         inches = distance_to_camera(known_width, focal_len, width)
-        if(inches < 0.06):
-            print("please slowdown ")
-            # while True:
-            #     print("please slowdown ")
-            #     if cv2.waitKey(1) & 0xFF == ord('q'):
-            #         break
+        if(inches < safe_distance):             #if the object is mush nearer  alert the driver about slowdown
+            print("please slowdown ")  
+            
 
-        if(eachObject["name"] == "person"):
-            speed = (inches - last_distance)/5
+        if(eachObject["name"] == "person"):  #here we are taking only person as object for prototyping but we on ground  we have take it as car, bus etc.
+            speed = (inches - last_distance)/5         #speed of the object is calculated as difference in distance and last calculated distance
             print("person speed :", speed)
 
-            if(speed - speed_us < -0.0001):
-                print("alert you may  it object ")
-                # while True:
-                #     print("please slowdown ")
-                #     if cv2.waitKey(1) & 0xFF == ord('q'):
-                #         break
+            if(speed <= -driver_speed):  #case when object is slowing down or in rest position
+                print("alert! object is rest or  it is moving opposite to you ")
+            else if(speed <0 ):
+                print("alert! object is slowing down")
+                
+                
+                
             last_distance = inches
         print(eachObject["name"], " : ", eachObject["percentage_probability"],
               " : ", eachObject["box_points"], "width :", width, " distance : ", inches)
